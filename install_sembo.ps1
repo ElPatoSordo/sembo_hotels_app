7b00f0d5b815562c1f7996f39251fb9f56753c96
ElPatoSordo <amat96pedro@gmail.com>
2021-02-22 19:57:02 +0100

    Added install powershell script.

---
+++ install_sembo.ps1
@@ -0,0 +1,8 @@
+﻿ren hotel_stats_api/.env.example .env
+composer install -d ./hotel_stats_api
+cp hotel_stats_api ../hotel_stats_api -Recurse
+cd ./hotel_stats_client
+npm install
+npm run build
+cd ..
+cp hotel_stats_client/build ../hotel_stats_client -Recurse
