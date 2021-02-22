c2efc63b5fed94e11cabc4a61caf00db68e5f4d8
ElPatoSordo <amat96pedro@gmail.com>
2021-02-22 17:49:18 +0100

    Configuration changes

--- hotel_stats_client/src/axios.js
+++ hotel_stats_client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost/sembo_hotels_app/hotel_stats_api/src/',
+  baseURL: 'http://localhost/hotel_stats_api/src/',
 });
 
 export default instance;
